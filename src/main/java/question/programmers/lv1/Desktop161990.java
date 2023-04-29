cd702829a64bab5f682d2ee3dea4afa93d51e868
kook1932 <gkswjddyd4@naver.com>
2023-04-29 23:26:01 +0900

    PRG Lv1 Desktop - 161990 Complete

--- src/main/java/question/programmers/lv1/Desktop161990.java
+++ src/main/java/question/programmers/lv1/Desktop161990.java
@@ -1,23 +1,32 @@
 package question.programmers.lv1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Desktop161990 {
 
 	public int[] solution(String[] wallpaper) {
-		int[] answer = {};
-		String[][] map = new String[wallpaper.length][wallpaper[0].length()];
+		int[] answer = new int[4];
+
+		int minX = wallpaper[0].length() - 1;
+		int maxX = 0;
+		int minY = wallpaper.length - 1;
+		int maxY = 0;
 
 		for (int i=0; i<wallpaper.length; i++){
 			for (int ii=0;ii<wallpaper[i].length(); ii++){
-				map[i][ii] = Character.toString(wallpaper[i].charAt(ii));
+				String file = Character.toString(wallpaper[i].charAt(ii));
+				if("#".equals(file)) {
+					minX = Math.min(minX, ii);
+					maxX = Math.max(maxX, ii);
+					minY = Math.min(minY, i);
+					maxY = Math.max(maxY, i);
+				}
 			}
 		}
-
-		for(int i=0;i<map.length;i++){
-			for(int ii=0; ii<map[i].length; ii++){
-
-			}
-		}
-
+		answer[0] = minY; answer[1] = minX;
+		answer[2] = maxY + 1; answer[3] = maxX + 1;
 
 		return answer;
 	}
